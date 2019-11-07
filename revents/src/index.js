58aa7b8fa87c3a4d22d41e31726aaa57fdc4f53f
saugat97 <saugat.thapa97@gmail.com>
2019-11-07 00:22:35 -0600

    Forms and Validations fn

--- revents/src/index.js
+++ revents/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { Provider } from "react-redux";
 import App from "./app/layout/App";
-import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 import { configureStore } from "./app/store/configureStore";
 import ScrollToTop from './app/common/util/ScrollToTop'
@@ -33,7 +32,3 @@ if (module.hot) {
 }
 render();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
