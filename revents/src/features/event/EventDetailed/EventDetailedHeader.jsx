2027e9dfae058d1a1e9904136793c354ec7a69e3
saugat97 <saugat.thapa97@gmail.com>
2019-11-16 17:45:42 -0600

    date-fns, redux-toastr implemented

--- revents/src/features/event/EventDetailed/EventDetailedHeader.jsx
+++ revents/src/features/event/EventDetailed/EventDetailedHeader.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Segment, Item, Header, Image, Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import {format, parseISO} from 'date-fns';
 
 const eventImageStyle = {
   filter: "brightness(30%)"
@@ -34,7 +35,7 @@ const EventDetailedHeader = ({ event }) => {
                   content={event.title}
                   style={{ color: "white" }}
                 />
-                <p>{event.date}}</p>
+                <p>{event.date && format(parseISO(event.date), 'do LLLL EEEE')}</p>
                 <p>
                   Hosted by <strong>{event.hostedBy}}</strong>
                 </p>
--- revents/src/index.js
+++ revents/src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import 'react-redux-toastr/lib/css/react-redux-toastr.min.css'
 import "./index.css";
 import { Provider } from "react-redux";
+import ReduxToastr from 'react-redux-toastr';
 import App from "./app/layout/App";
 import { BrowserRouter } from "react-router-dom";
 import { configureStore } from "./app/store/configureStore";
@@ -18,6 +20,11 @@ let render = () => {
     <Provider store={store}>
       <BrowserRouter>
        <ScrollToTop>
+         <ReduxToastr 
+            position='bottom-right'
+            transitionIn='fadeIn'
+            transitionOut='fadeOut'
+         />
           <App />
        </ScrollToTop>
        
