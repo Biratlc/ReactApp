a77bfb384bcfcd1f8896b4dbf2a2a527651a7d66
saugat97 <saugat.thapa97@gmail.com>
2019-11-19 15:14:58 -0600

    Revert "Revert "Merge pull request #1 from saugat97/till-now""
    This reverts commit fd425681ff632b7832f6d0bad9683a9e406b7546.

--- revents/src/features/auth/Register/RegisterForm.jsx
+++ revents/src/features/auth/Register/RegisterForm.jsx
@@ -17,7 +17,7 @@ const validate = combineValidators({
   password: isRequired('password')
 })
 
-const RegisterForm = ({handleSubmit, registerUser, error, invalid, submitting}) => {
+const RegisterForm = ({handleSubmit, registerUser, error, invalid, submitting, loading}) => {
   return (
     <div>
       <Form size="large" onSubmit={handleSubmit(registerUser)}
@@ -41,14 +41,21 @@ const RegisterForm = ({handleSubmit, registerUser, error, invalid, submitting}) 
             component={TextInput}
             placeholder="Password"
           />
-          {error && <Label basic color='red'>{error}</Label>}
-          <Button disbaled={invalid || submitting} fluid size="large" color="teal">
+          {error && (
+            <Label basic color='red'>
+              {error}
+            </Label>
+          )}
+          <Button
+            disabled={invalid || submitting}
+            fluid
+            size='large'
+            color='teal'
+          >
             Register
           </Button>
-          <Divider horizontal>
-          Or
-          </Divider>
-          <SocialLogin/>
+          <Divider horizontal>Or</Divider>
+          <SocialLogin />
         </Segment>
       </Form>
     </div>
