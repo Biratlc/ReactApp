b3005688ca0e46da87e1c1f11282e0f878411dcc
saugat97 <saugat.thapa97@gmail.com>
2019-11-19 15:15:01 -0600

    Revert "Merge pull request #1 from saugat97/till-now"
    This reverts commit 325b2eba383b3f6838b176cdd74d1639ae4c1f5f, reversing
    changes made to c58547fce57944feb525b343795a79b265eccb4c.

--- revents/src/app/common/util/helpers.js
+++ revents/src/app/common/util/helpers.js
@@ -1,20 +1,16 @@
-export const objectToArray = (object) => {
-    if(object) {
-        return Object.entries(object).map(e => Object.assign({}, e[1], {id: e[0]}))
-    }
-}
+import { tryStatement } from "@babel/types"
 
 export const createNewEvent = (user, photoURL, event) => {
-    return {
+    return{
         ...event,
         hostUid: user.uid,
         hostedBy: user.displayName,
         hostPhotoURL: photoURL || '/assets/user.png',
         created: new Date(),
-        attendees: {
-            [user.uid]: {
-                going: true,
-                joinDate: new Date(),
+        attendees:{
+            [user.uid]:{
+                going: tryStatement,
+                joinDtae: new Date(),
                 photoURL: photoURL || '/assets/user.png',
                 displayName: user.displayName,
                 host: true
